0eec43e325e3ab3bd545fa7fbfe39050ccd5202f
Tejas <tejaskait@gmail.com>
2024-11-22 22:26:46 +0530

    created api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 const app = express();
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,3 +12,4 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.listen(3000, ()=> {
     console.log('listening on 3000!!');
 }); 
+app.use('/api/user', userRouter);
