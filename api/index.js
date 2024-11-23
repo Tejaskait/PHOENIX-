d9a8c48b785bafbd63e38f81ae39ea02c5a50ffe
Tejas <tejaskait@gmail.com>
2024-11-23 12:04:41 +0530

    create meal listings model

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 app.use(express.json());
@@ -20,6 +21,7 @@ app.listen(3000, ()=> {
 }); 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
