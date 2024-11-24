1a624421a244a18406b277edd0bc0df98a3dff71
Tejas <tejaskait@gmail.com>
2024-11-24 19:04:24 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 
@@ -23,6 +27,10 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname , '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
