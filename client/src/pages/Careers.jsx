69e2c91946eb3b94adeabfd44f1d16ec336047f8
Tejas <tejaskait@gmail.com>
2024-11-26 19:54:24 +0530

    careeers page

--- client/src/pages/Careers.jsx
+++ client/src/pages/Careers.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 export default function Careers() {
   return (
-    <div>Careers</div>
+    <div className='bg-center  
+     bg-cover bg-[url("https://i.pinimg.com/originals/7c/fd/76/7cfd76db5ddac00b668813877346078a.gif")]'> 
+     
+      <div className='flex justify-center items-center'>
+      <h1 className='py-20 text-white text-[6vw] font-nmregular'>
+      Jobs for "YOU"
+      </h1>
+      </div>
+      <div className='flex justify-center items-center text-[4vw]'>
+      <input
+          type="text"
+              placeholder='Search...'
+          className='bg-transparent  focus:outline-none w-24 sm:w-64'
+         
+        />
+        <button>
+            <FaSearch className='text-slate-600' />
+          </button>
+      </div>
+      </div>
   )
 }
